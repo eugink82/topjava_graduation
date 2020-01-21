f105f344b8555d47b6aa8ed9f844702a5ec64932
Евгений <eugink82@mail.ru>
2020-01-22 02:01:57 +0300

    S.19 Добавлена возможность просмотра голоса пользователя      Vote -DTO голоса пользователя      getMyVote -просмотр пользователем его голоса

---
+++ src/main/java/ru/graduation/to/VoteTo.java
@@ -0,0 +1,38 @@
+package ru.graduation.to;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+import ru.graduation.model.Vote;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class VoteTo {
+
+    @JsonIgnore
+    private Integer id;
+
+    private String email;
+
+    private LocalDate date;
+
+    private String name;
+
+    public VoteTo(Vote vote) {
+        this.id = vote.getId();
+        this.email=vote.getUser().getEmail();
+        this.date = vote.getVoteDate();
+        this.name = vote.getRestaurant().getName();
+    }
+
+    @Override
+    public String toString() {
+        return "VoteTo{" +
+                "email='" + email + '\'' +
+                ", date=" + date +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
--- src/main/java/ru/graduation/web/VoteController.java
+++ src/main/java/ru/graduation/web/VoteController.java
@@ -3,14 +3,17 @@ package ru.graduation.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.graduation.SecurityUtil;
 import ru.graduation.model.Vote;
 import ru.graduation.service.VoteService;
+import ru.graduation.to.VoteTo;
 
 import java.net.URI;
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping(VoteController.VOTE_URL)
@@ -29,6 +32,11 @@ public class VoteController {
         URI uriOfNewResource= ServletUriComponentsBuilder.fromCurrentContextPath()
                 .buildAndExpand(VOTE_URL+"/{newId}",id,created.getId()).toUri();
         return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public VoteTo getMyVote(){
+        return new VoteTo(service.getVoteByUserId(LocalDate.now(),SecurityUtil.authUserId()));
     }
 
 }
